12bd5c80b17e7524ec3abcb7bb0f1338f3363b66
ndjerrou <n.djerrou@gmail.com>
2024-03-25 16:16:57 +0000

    first exo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = createRoot(document.querySelector('#root'));
+
+// JSX - Javascript XML
+const name = 'Pierre';
+const age = 18;
+
+const pickBackgroundColor = age => {
+  return age >= 18 ? 'blue' : 'green';
+};
+
+const template = (
+  <div>
+    <h1
+      id='title'
+      style={{
+        color: age >= 18 ? 'white' : 'black',
+        backgroundColor: pickBackgroundColor(age),
+      }}
+    >
+      My hobbies by {name}
+    </h1>
+    <ul>
+      <li>Foot</li>
+      <li>Roller</li>
+    </ul>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(template);
